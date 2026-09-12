802995ba60fcc2710d0cefe06cae6431b8e3cb04
Wansti <wansti@discarded-ideas.org>
2026-09-12 18:17:38 +1000

    Add initial lazyvim config

---
+++ config/nvim/lua/plugins/theme.lua
@@ -0,0 +1,52 @@
+return {
+  {
+    "bjarneo/aether.nvim",
+    branch = "v3",
+    name = "aether",
+    priority = 1000,
+    opts = {
+      colors = {
+        bg = "#212337",
+        dark_bg = "#191a29",
+        darker_bg = "#11121c",
+        lighter_bg = "#212337",
+
+        fg = "#ebfafa",
+        dark_fg = "#21222c",
+        light_fg = "#ebfafa",
+        bright_fg = "#ebfafa",
+        muted = "#77777c",
+
+        red = "#f9515d",
+        yellow = "#e9f941",
+        orange = "#e9f941",
+        green = "#37f499",
+        cyan = "#04d1f9",
+        blue = "#9071f4",
+        magenta = "#f265b5",
+        brown = "#757d21",
+
+        bright_red = "#f9515d",
+        bright_yellow = "#e9f941",
+        bright_green = "#37f499",
+        bright_cyan = "#04d1f9",
+        bright_blue = "#9071f4",
+        bright_magenta = "#f265b5",
+
+        accent = "#9071f4",
+        cursor = "#ebfafa",
+        foreground = "#ebfafa",
+        background = "#212337",
+        selection = "#bf4f8e",
+        selection_foreground = "#ebfafa",
+        selection_background = "#bf4f8e",
+      },
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "aether",
+    },
+  },
+}
